2cf3fefa635b629a499ba99bf4a226d1b09a21c3
Ike Orisakwe <IkeOrisakwe@gmail.com>
2020-09-23 15:04:47 -0500

    update DOM PRACTICE

---
+++ domManipulationPractice.js
@@ -0,0 +1,12 @@
+// to select the element "button" to get your button to function we go here to the javascript by creating a variable and intializing it with the value of document.
+// create a variable 
+// initialize it by issuing it a value of document.quereySelector
+
+
+    // METHOD - Returns the first element that is a descendant of node that matches selectors.
+    // what are some other options available for the METHOD? 
+        //answer?: (method) ParentNode.querySelector<K extends "object" | "a" | "abbr" | "address" | "applet" | "area" | "article" | "aside" | "audio" | "b" | "base" | "basefont" | "bdi" | "bdo" | "blockquote" | "body" | "br" | "button" | "canvas" | ... 99 more ... | "wbr">(selectors: K): HTMLElementTagNameMap[K] (+2 overloads).  Returns the first element that is a descendant of node that matches selectors.
+//querySelector -  The querySelector() method of the Element interface returns the first element that is a descendant of the element on which it is invoked that matches the specified group of selectors.
+// SYNTAX :element = baseElement.querySelector(selectors);
+     
+const text = document.querySelector
--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log('Add validation!');
+
+// console log from QuereySlector using an ID 
+//
+
+let form = document.querySelector("#parking-form")
+console.log(form)
+
+    
+//this is a callback function. specifically the submit is the event. after the comma is the callback function
+form.addEventListener("submit", (event) => {
+    //when submit is triggered the things below will happen
+    event.preventDefault();
+    console.log ("hello")
+    let name = document.querySelector("#name")
+    if (name.value.length === 0) {
+        console.log("Name Is Blank");
+
+    } else {
+        console.log("valid")
+    }
+} )
+
+
+
+function validateForm(){
+const form = document.querySelector("#parking-form")
+form.addEventListener("submit")
+
+}
+// we want to validate the number but first we have to GET the number
+// you get the conext of a n input value by using .value
+// how to declare a variable 
+//focus in of Queuery selecctors and DOM manipulation. 
+// preventDefault = prevent the default action and perform another
+// referesh what is the === and ++ 
+// what is the && symbols used for 
+// to make sur3e the inupt is a number you use "type" input id="car-model"
+// Look up ParseInt - used to transform something into an integer PArse... integer
