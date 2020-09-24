2a9e73160c68f81d3d4f9142279985e687ee5cec
Ike Orisakwe <IkeOrisakwe@gmail.com>
2020-09-24 13:12:06 -0500

    update

--- domManipulationPractice.js
+++ domManipulationPractice.js
@@ -1,12 +1,70 @@
+
+// use . to refer to Classes 
+// use # to refer to IDs
+const text = document.querySelector(".title"); 
+const changeColor = document.querySelector(".changeColor");
+
+text.style.color = "teal";
+text.style.backgroundColor = "beige", "black";
+text.style.transfor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //                      vocab word refresher/ research
+
+// METHOD ?- Returns the first element that is a descendant of node that matches selectors.
+
+// querySelector -  The querySelector() method of the Element interface returns the first element that is a descendant of the element on which it is invoked that matches the specified group of selectors.
+// SYNTAX :element = baseElement.querySelector(selectors);
+// ParentNode - 
+// 
+
+
+
+
+
+//                      VOCAB WORD REFRESHER /REASEARCH
+
+
+
+
+
+
+
 // to select the element "button" to get your button to function we go here to the javascript by creating a variable and intializing it with the value of document.
 // create a variable 
 // initialize it by issuing it a value of document.quereySelector
 
 
-    // METHOD - Returns the first element that is a descendant of node that matches selectors.
-    // what are some other options available for the METHOD? 
-        //answer?: (method) ParentNode.querySelector<K extends "object" | "a" | "abbr" | "address" | "applet" | "area" | "article" | "aside" | "audio" | "b" | "base" | "basefont" | "bdi" | "bdo" | "blockquote" | "body" | "br" | "button" | "canvas" | ... 99 more ... | "wbr">(selectors: K): HTMLElementTagNameMap[K] (+2 overloads).  Returns the first element that is a descendant of node that matches selectors.
-//querySelector -  The querySelector() method of the Element interface returns the first element that is a descendant of the element on which it is invoked that matches the specified group of selectors.
-// SYNTAX :element = baseElement.querySelector(selectors);
      
-const text = document.querySelector
+// here in our javascript, we can get those ELEMENTS from our html page by creating a variable and initializing it with a value of document.querySelector
+
+// with querySelector we are grabbing the ELEMENT and storing it inside of the variable. 
